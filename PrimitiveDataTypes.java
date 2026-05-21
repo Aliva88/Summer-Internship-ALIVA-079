d7b806a2ca0a5a1b1b951141e3d95e26f9b46511
ALIVA SAHOO <alivasahoo66gmail.com>
2026-05-21 19:41:15 +0530

    message

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+class PrimitiveDataTypes
+{
+    public static void main(String args[])
+    {
+        byte b = 10;
+        short s = 200;
+        int i = 5000;
+        long l = 100000L;
+        float f = 12.5f;
+        double d = 123.456;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("Byte Value = " + b);
+        System.out.println("Short Value = " + s);
+        System.out.println("Int Value = " + i);
+        System.out.println("Long Value = " + l);
+        System.out.println("Float Value = " + f);
+        System.out.println("Double Value = " + d);
+        System.out.println("Char Value = " + c);
+        System.out.println("Boolean Value = " + bool);
+    }
+}
